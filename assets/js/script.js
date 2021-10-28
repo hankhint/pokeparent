5fa6e1f1563000359355cee000735fb7808e04aa
HankHint <hankhint@gmail.com>
2021-10-27 21:26:23 -0400

    linked js with index html

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+
+// var getCityWeather = function (city) {
+//     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=67134688628a630443996290ce30ced4'
+//     fetch(apiUrl)
+//     .then(function (response) {
+//         if (response.ok) {
+//             console.log(response);
+//             response.json().then(function (data) {
+//                 console.log(data);
+//             });
+//         }
+//     });
+//  };
+
+//  getCityWeather();
