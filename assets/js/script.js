52db472880e2501ba670e126038dd4fd951b94c8
Bryan A <bryanacostadelacruz@gmail.com>
2021-10-31 14:09:13 -0500

    Added Shinys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+//FUNctions
+function probability(n){
+  return Math.random() < n;
+}
 //To get a Pokemons data ---> <div class="PokeData" id="bulbasaur"> <CODE BLOCK> </div>
 var getPokeApi = function() {
   var PokeDataEl = document.querySelectorAll(".PokeData");
@@ -24,9 +28,20 @@ function PokeCommands(data){
     PokeNameEl.textContent = data.name;
     PokeNameEl.id = '';
 
+  //var PokePicGifEl = document.querySelector("#PokePicGif");
+  //  PokePicGifEl.src = "https://projectpokemon.org/images/normal-sprite/" + data.name + ".gif";//data.sprites.front_default;
+  //  PokePicGifEl.alt = data.name;
+  // PokePicGifEl.id = '';
+
 //<img id ="PokePic" src='leave empty' ></p>
   var PokePicEl = document.querySelector("#PokePic");
-    PokePicEl.src = data.sprites.front_default;
+    if(probability(0.0001220703125)){
+      PokePicEl.src = data.sprites.front_shiny;
+      var shiny = true;
+    }
+    else{
+      PokePicEl.src = data.sprites.front_default;
+    }
     PokePicEl.alt = data.name;
     PokePicEl.id = '';
 }
