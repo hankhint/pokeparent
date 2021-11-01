c8562c0b2acce48b5071b05138610a5b27d4de6d
Bryan A <bryanacostadelacruz@gmail.com>
2021-11-01 13:35:35 -0500

    finished enemy and player stats and info boxes

--- assets/js/fight.js
+++ assets/js/fight.js
@@ -4,6 +4,7 @@ function probability(n){
   }
   //To get a Pokemons data ---> <div class="PokeData" id="bulbasaur"> <CODE BLOCK> </div>
   var getPokeApi = function() {
+
     var PokeDataEl = document.querySelector(".PokeEnemy");
         var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + PokeDataEl.id;
         fetch(apiUrl).then(function(response) {
@@ -11,28 +12,30 @@ function probability(n){
             enemypokemon(data);
           });
         });
+
+        var PokeDataEl = document.querySelector(".PokePlayer");
+        var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + PokeDataEl.id;
+        fetch(apiUrl).then(function(response) {
+          response.json().then(function(data) {
+            playerpokemon(data);
+          });
+        });
   };
   
   
   function enemypokemon(data){
-    /*
-    if(PokePicGifEl.className == 'playerpokemon'){
-        PokePicGifEl.src = "https://projectpokemon.org/images/sprites-models/normal-back/" + data.name + ".gif";
-        PokePicGifEl.alt = data.name;
-        PokePicGifEl.id = '';
-      }
-      */
     var img = "https://projectpokemon.org/images/normal-sprite/" + data.name + ".gif";
     var name = data.name; 
     var PokeStats = data.stats;
     var level = 20;
-    var hp = Math.floor(0.01 * (2 * PokeStats[0].base_stat ) * level) + level + 10;
+    var Maxhp = Math.floor(0.01 * (2 * PokeStats[0].base_stat ) * level) + level + 10;
     var atk = Math.floor(0.01 * (2 * PokeStats[1].base_stat) * level) + 5;
     var def = Math.floor(0.01 * (2 * PokeStats[2].base_stat) * level) + 5;
     var spAtk = Math.floor(0.01 * (2 * PokeStats[3].base_stat) * level) + 5;
     var spDef = Math.floor(0.01 * (2 * PokeStats[4].base_stat) * level) + 5;
     var speed = Math.floor(0.01 * (2 * PokeStats[5].base_stat) * level) + 5;
-    var enemystats = [level,hp,atk,def,spAtk,spDef,speed];
+    var hp = Maxhp;
+    var enemystats = [level,Maxhp,hp,atk,def,spAtk,spDef,speed];
 
       enemyFill(name, img, enemystats);
   }
@@ -41,6 +44,9 @@ function probability(n){
       console.log(name);
       console.log(img);
       console.log(stats);
+
+      var enemyhpbarEl = document.querySelector("#enemyhpbar");
+      enemyhpbarEl.style.width= ((stats[2]/stats[1])*100) + "px";
 
       var enemygifEl = document.querySelector("#enemygif");
       enemygifEl.src = img;
@@ -51,4 +57,40 @@ function probability(n){
       var enemyLvEl = document.querySelector("#enemylv");
       enemyLvEl.textContent = "Lv"+stats[0];
   }
+
+  function playerpokemon(data){
+    var img = "https://projectpokemon.org/images/sprites-models/normal-back/" + data.name + ".gif";
+    var name = data.name; 
+    var PokeStats = data.stats;
+    var level = 20;
+    var Maxhp = Math.floor(0.01 * (2 * PokeStats[0].base_stat ) * level) + level + 10;
+    var atk = Math.floor(0.01 * (2 * PokeStats[1].base_stat) * level) + 5;
+    var def = Math.floor(0.01 * (2 * PokeStats[2].base_stat) * level) + 5;
+    var spAtk = Math.floor(0.01 * (2 * PokeStats[3].base_stat) * level) + 5;
+    var spDef = Math.floor(0.01 * (2 * PokeStats[4].base_stat) * level) + 5;
+    var speed = Math.floor(0.01 * (2 * PokeStats[5].base_stat) * level) + 5;
+    var hp = Maxhp;
+    var playerstats = [level,Maxhp,hp,atk,def,spAtk,spDef,speed];
+
+      playerFill(name, img, playerstats);
+  }
+
+  function playerFill(name, img, stats){
+    console.log(name);
+    console.log(img);
+    console.log(stats);
+
+    var playerhpbarEl = document.querySelector("#playerhpbar");
+    playerhpbarEl.style.width= ((stats[2]/stats[1])*100) + "px";
+
+    var playergifEl = document.querySelector("#playergif");
+    playergifEl.src = img;
+
+    var playernameEl = document.querySelector("#playername");
+    playernameEl.textContent = name.toUpperCase();
+
+    var playerLvEl = document.querySelector("#playerlv");
+    playerLvEl.textContent = "Lv"+stats[0];
+}
+
   getPokeApi();
